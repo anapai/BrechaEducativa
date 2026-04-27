2e1887143e668fa8ebd6b81a8ea716a9f18d8242
ANA PAU <193064-0@iberoleon.edu.mx>
2026-04-26 22:26:33 -0600

    Grafica 3

--- app/app.py
+++ app/app.py
@@ -1,7 +1,7 @@
 import pandas as pd
-import matplotlib.pyplot as plt 
-df = pd.read_csv("data/serie_historica_entidades_sep.csv", encoding="latin1")
+import matplotlib.pyplot as plt
 
+df = pd.read_csv("data/serie_historica_entidades_sep.csv", encoding="latin1")
 df.columns = df.columns.str.strip()
 
 df_long = df.melt(
@@ -22,31 +22,39 @@ df_long["Tipo"] = df_long["Tipo"].replace({
 })
 
 df_filtrado = df_long[df_long["Tipo"].isin(["PUBLICO", "PRIVADO"])]
-'''
+
 #propuesta grafica 1
 df_grafica = df_filtrado.groupby(["Año", "Tipo"])["Matricula"].sum().reset_index()
 for tipo in df_grafica["Tipo"].unique():
     data = df_grafica[df_grafica["Tipo"] == tipo]
     plt.plot(data["Año"], data["Matricula"], label=tipo)
-
 plt.title("Matrícula en México: Público vs Privado")
 plt.xlabel("Año")
 plt.ylabel("Matrícula")
 plt.legend()
-plt.show()
-
-'''
 
 
 
 
+
+
+#2
 df_pivot = df_filtrado.groupby(["Sector", "Tipo"])["Matricula"].sum().unstack()
-
 df_pivot.plot(kind="bar")
-
 plt.title("Matrícula por Nivel Educativo: Público vs Privado")
 plt.xlabel("Nivel Educativo")
 plt.ylabel("Matrícula")
 plt.xticks(rotation=45)
 
-plt.show()
+
+#3
+df_estado = df_filtrado.groupby(["Estado", "Tipo"])["Matricula"].sum().unstack()
+df_estado["Diferencia"] = df_estado["PUBLICO"] - df_estado["PRIVADO"]
+df_estado = df_estado.sort_values(by="Diferencia", ascending=False)
+top_estados = df_estado.head(10)
+top_estados["Diferencia"].plot(kind="bar")
+plt.title("Estados con mayor brecha educativa (Público - Privado)")
+plt.xlabel("Estado")
+plt.ylabel("Diferencia de Matrícula")
+plt.xticks(rotation=45)
+
